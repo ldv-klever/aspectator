c15b5e2e5af0ad76a2433310f6dd0d0900690db0
Evgeny Novikov <joker@ispras.ru>
2012-03-29 13:31:50 +0000

    Insert copyright notices for aspectator core

--- libcpp/include/ldv-cpp-pointcut-matcher.h
+++ libcpp/include/ldv-cpp-pointcut-matcher.h
@@ -1,3 +1,21 @@
+/* Copyright (C) 2009-2012
+   Institute for System Programming, Russian Academy of Sciences (ISPRAS).
+
+This file is part of C Instrumentation Framework.
+
+C Instrumentation Framework is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+C Instrumentation Framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+C Instrumentation Framework.  If not, see <http://www.gnu.org/licenses/>.  */
+
 #ifndef _LDV_CPP_POINTCUT_MATCHER_H_
 #define _LDV_CPP_POINTCUT_MATCHER_H_
 
