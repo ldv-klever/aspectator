eaed81dcbd2e0d7aa0f61586a5371a39367335a4
Evgeny Novikov <joker@ispras.ru>
2012-09-07 15:29:37 +0000

    Processed TRUTH_AND_EXPR and TRUTH_OR_EXPR (#1159)

--- gcc/ldv-pointcut-matcher.c
+++ gcc/ldv-pointcut-matcher.c
@@ -301,6 +301,8 @@ ldv_match_expr (tree t)
         case COMPOUND_EXPR:
         case TRUTH_ANDIF_EXPR:
         case TRUTH_ORIF_EXPR:
+        case TRUTH_AND_EXPR:
+        case TRUTH_OR_EXPR:
         case TRUTH_XOR_EXPR:
           ldv_match_expr (LDV_OP1);
           ldv_match_expr (LDV_OP2);
