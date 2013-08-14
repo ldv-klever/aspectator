c0df63e6e59b1a5a91260cefa38b89a56cc549ec
Ilya Shchepetkov <shchepetkov@ispras.ru>
2013-08-14 17:55:10 +0400

    Added support of $func_context_path aspect body pattern

--- gcc/ldv-advice-weaver.c
+++ gcc/ldv-advice-weaver.c
@@ -107,6 +107,7 @@ static void ldv_print_direct_declarator (ldv_list_ptr);
 static void ldv_print_int (int);
 static const char *ldv_print_func_context (ldv_i_func_ptr);
 static const char *ldv_print_func_context_name (ldv_i_func_ptr);
+static const char *ldv_print_func_context_path (ldv_i_func_ptr);
 static const char *ldv_print_func_path (ldv_i_func_ptr);
 static const char *ldv_print_func_signature (ldv_pps_decl_ptr);
 static void ldv_print_macro_name (ldv_id_ptr);
@@ -393,6 +394,22 @@ ldv_evaluate_aspect_pattern (ldv_aspect_pattern_ptr pattern, const char **string
         {
           if (ldv_func_signature->func_context)
             text = ldv_print_func_context_name(ldv_func_signature);
+          else
+            {
+              LDV_FATAL_ERROR ("no function context was found for aspect pattern \"%s\"", pattern->name);
+            }
+        }
+      else
+        {
+          LDV_FATAL_ERROR ("no function signature was found for aspect pattern \"%s\"", pattern->name);
+        }
+    }
+  else if (!strcmp (pattern->name, "func_context_path"))
+    {
+      if (ldv_func_signature)
+        {
+          if (ldv_func_signature->func_context)
+            text = ldv_print_func_context_path(ldv_func_signature);
           else
             {
               LDV_FATAL_ERROR ("no function context was found for aspect pattern \"%s\"", pattern->name);
@@ -1215,6 +1232,12 @@ const char *
 ldv_print_func_context_name (ldv_i_func_ptr decl)
 {
   return ldv_get_id_name (decl->func_context->name);
+}
+
+const char *
+ldv_print_func_context_path (ldv_i_func_ptr decl)
+{
+  return ldv_print_func_path (decl->func_context);
 }
 
 const char *
