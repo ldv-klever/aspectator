ldv-klever/aspectator

commit ad1b517367f940eb3a3c8a443f3b76f88c656dc5

Fix complicated memleak in ldv_weave_advice()
Introduce two new functions to copy internal representation for parameters and types.

libcpp/ldv-cpp-pointcut-matcher.c
@@ -439,7 +439,9 @@ ldv_match_func_signature (ldv_i_match_ptr i_match, ldv_pps_decl_ptr pps_func)
   /* Replace aspect function name used just for a current matching with the source
      one since they match each other but the aspect one can contain '$'
      wildcards.*/
-  func_aspect->name = func_source->name;
+  ldv_free_id (func_aspect->name);
+  func_aspect->name = ldv_create_id ();
+  ldv_puts_id (ldv_cpp_get_id_name (func_source->name), func_aspect->name);
 
   /* Specify that a function was matched by name. */
   i_match->ismatched_by_name = true;
@@ -1041,7 +1043,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                         && matching_table_coord->x != matching_table_coord_next->x)
                         || (!matching_table_coord_next && matching_table_coord_prev))
                         {
-                          param_second_list = ldv_list_insert_data (param_second_list, params_first[j]);
+                          param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (param_second));
                           j++;
                         }
 
@@ -1062,7 +1064,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                      arguments list. */
                   else if (matching_table_coord->y == matching_table_coord_prev->y)
                     {
-                      param_second_list = ldv_list_insert_data (param_second_list, params_first[j]);
+                      param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (param_second));
                       j++;
 
                       /* Shift an aspect function argument when a next true
@@ -1105,7 +1107,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                              wildcard. */
                           if (matching_table_coord->x != matching_table_coord_next->x)
                             {
-                              param_second_list = second->param = ldv_list_push_front (second->param, params_first[j]);
+                              param_second_list = second->param = ldv_list_push_front (second->param, ldv_copy_iparam (params_first[j]));
                               j++;
 
                               /* Finalize a '..' wildcard. */
@@ -1127,7 +1129,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                          source function argument. */
                       else
                         {
-                          ldv_list_push_back (&second->param, params_first[j]);
+                          ldv_list_push_back (&second->param, ldv_copy_iparam (params_first[j]));
                           param_second_list = second->param;
                           param_second_list = ldv_list_get_next (param_second_list);
                           i++;

gcc/ldv-advice-weaver.c
@@ -2021,7 +2021,7 @@ ldv_weave_advice (expanded_location *open_brace, expanded_location *close_brace)
         }
 
       ldv_free_info_func (func_source);
-      /* TODO: deal with number of memory problems: ldv_free_info_func (func_aspect); */
+      ldv_free_info_func (func_aspect);
 
       break;
 

libcpp/include/ldv-cpp-core.h
@@ -57,6 +57,8 @@ extern ldv_i_typedecl_ptr ldv_create_info_typedecl (void);
 extern ldv_i_var_ptr ldv_create_info_var (void);
 
 extern ldv_pps_declspecs_ptr ldv_copy_declspecs (ldv_pps_declspecs_ptr);
+extern ldv_i_param_ptr ldv_copy_iparam (ldv_i_param_ptr);
+extern ldv_i_type_ptr ldv_copy_itype (ldv_i_type_ptr);
 
 extern void ldv_free_declspecs (ldv_pps_declspecs_ptr);
 extern void ldv_free_info_func (ldv_i_func_ptr);

libcpp/ldv-cpp-core.c
@@ -479,6 +479,26 @@ ldv_create_info_param (void)
   return param;
 }
 
+ldv_i_param_ptr
+ldv_copy_iparam (ldv_i_param_ptr iparam)
+{
+  ldv_i_param_ptr iparam_new = NULL;
+
+  iparam_new = ldv_create_info_param ();
+
+  /* Copy parameter name. */
+  iparam_new->name = ldv_create_id ();
+  ldv_puts_id (ldv_cpp_get_id_name (iparam->name), iparam_new->name);
+
+  /* Copy parameter type. */
+  iparam_new->type = ldv_copy_itype (iparam->type);
+
+  /* Copy the rest of parameter information. */
+  iparam_new->isany_params = iparam->isany_params;
+
+  return iparam_new;
+}
+
 void
 ldv_free_info_param (ldv_i_param_ptr param)
 {
@@ -500,6 +520,52 @@ ldv_create_info_type (void)
   type->it_kind = LDV_IT_NONE;
 
   return type;
+}
+
+ldv_i_type_ptr
+ldv_copy_itype (ldv_i_type_ptr itype)
+{
+  ldv_i_type_ptr itype_new = NULL;
+  ldv_list_ptr param_list = NULL;
+  ldv_i_param_ptr iparam = NULL;
+
+  itype_new = ldv_create_info_type ();
+
+  itype_new->it_kind = itype->it_kind;
+
+  if (itype->primitive_type)
+    itype_new->primitive_type = ldv_copy_declspecs (itype->primitive_type);
+
+  /* Copy array element types and the number of elements. */
+  if (itype->array_type)
+    itype_new->array_type = ldv_copy_itype (itype->array_type);
+  itype_new->array_size = itype->array_size;
+  itype_new->issize_specified = itype->issize_specified;
+  itype_new->isany_size = itype->isany_size;
+
+  /* Copy pointed type. */
+  if (itype->ptr_type)
+    itype_new->ptr_type = ldv_copy_itype (itype->ptr_type);
+  itype_new->isconst = itype->isconst;
+  itype_new->isvolatile = itype->isvolatile;
+  itype_new->isrestrict = itype->isrestrict;
+
+  /* Copy function return type and argument types. */
+  if (itype->ret_type)
+    itype_new->ret_type = ldv_copy_itype (itype->ret_type);
+  if (itype->param)
+    {
+      for (param_list = itype->param
+        ; param_list
+        ; param_list = ldv_list_get_next (param_list))
+        {
+          iparam = (ldv_i_param_ptr) ldv_list_get_data (param_list);
+          ldv_list_push_back (&itype_new->param, ldv_copy_iparam (iparam));
+        }
+    }
+  itype_new->isva = itype->isva;
+
+  return itype_new;
 }
 
 void

commit 92931a1048ea777bfd768a23a316cac0c1f17f08

Fix bugs in ldv_match_type() introduced in commit ad1b517

libcpp/ldv-cpp-pointcut-matcher.c
@@ -1076,7 +1076,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                         && matching_table_coord->x != matching_table_coord_next->x)
                         || (!matching_table_coord_next && matching_table_coord_prev))
                         {
-                          param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (param_second));
+                          param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (params_first[j]));
                           j++;
                         }
 
@@ -1097,7 +1097,7 @@ ldv_match_type (ldv_i_type_ptr first, ldv_i_type_ptr second)
                      arguments list. */
                   else if (matching_table_coord->y == matching_table_coord_prev->y)
                     {
-                      param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (param_second));
+                      param_second_list = ldv_list_insert_data (param_second_list, ldv_copy_iparam (params_first[j]));
                       j++;
 
                       /* Shift an aspect function argument when a next true