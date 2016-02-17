94e22f3d10f8e507e0ebd7d18ebd1887319420a2
Ilya Shchepetkov <shchepetkov@ispras.ru>
2016-02-17 15:06:34 +0300

    Add support of "use_func" request for global variables

--- gcc/ldv-pointcut-matcher.c
+++ gcc/ldv-pointcut-matcher.c
@@ -1219,7 +1219,10 @@ ldv_match_var (tree t, ldv_ppk pp_kind)
   /* Convert variable declaration initializer to internal representation.
      Do this just for structure variables. */
   if (TREE_CODE (t) == VAR_DECL && DECL_INITIAL (t))
-    var->initializer_list = ldv_convert_initializer_to_internal (DECL_INITIAL (t));
+    {
+      var->initializer_list = ldv_convert_initializer_to_internal (DECL_INITIAL (t));
+      ldv_match_expr(t);
+    }
 
   /* Walk through an advice definitions list to find matches. */
   for (adef_list = ldv_adef_list; adef_list; adef_list = ldv_list_get_next (adef_list))
